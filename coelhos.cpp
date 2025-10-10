9aabce38155e5c93331ae85033be5214f4807845
Yuuki Hara <nathan.hara@escola.pr.gov.br>
2025-10-10 11:34:33 -0300

    Update coelhos.cpp

--- coelhos.cpp
+++ coelhos.cpp
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+    int geracoes;  
+    int coelhos = 1;  
+
+   
+    printf("Digite o número de gerações: ");
+    scanf("%d", &geracoes);
+
+    
+    for (int i = 1; i <= geracoes; i++) {
+        printf("Geração %d: %d coelhos\n", i, coelhos);
+        coelhos *= 2;  
+    }
+}
+
+  
