4a971c78d88103aaae133f8d5dae3e4c06eabbd4
Yuuki Hara <nathan.hara@escola.pr.gov.br>
2025-10-17 12:22:02 -0300

    Update Asteriscos_fudidos.cpp

--- Asteriscos_fudidos.cpp
+++ Asteriscos_fudidos.cpp
@@ -1,1 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+	int linhas_legais;
+	
+
+	printf("digite a quantidade de linhas por favorzinho da silva:");
+	scanf("%d", &linhas_legais);
+
+	for(int qtd_linhas= 0; qtd_linhas <= linhas_legais; qtd_linhas++) {
+		for(int espacos_fudidos = qtd_linhas; espacos_fudidos <= linhas_legais - 1;espacos_fudidos++ ){
+		   printf(" ");
+		}
+		for(int asteriscos_fudidos = 1; asteriscos_fudidos <= qtd_linhas + qtd_linhas - 1; asteriscos_fudidos++) {
+		   printf("*");
+		}
+		printf("\n");
+	}
+
+
+
+}
 
