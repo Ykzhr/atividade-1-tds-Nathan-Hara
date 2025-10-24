20f1b90b8591cef92dfccedc6d49aed6f85b8115
Yuuki Hara <nathan.hara@escola.pr.gov.br>
2025-10-24 12:27:30 -0300

    Update Triangulo de Floyd.cpp

--- Triangulo de Floyd.cpp
+++ Triangulo de Floyd.cpp
@@ -1,1 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int linhas_legais;
+    int numerozinho = 1;
+
+    
+    printf("Digite a quantidade de linhas, por favorzinho da Silva: ");
+    scanf("%d", &linhas_legais);
+
+    for (int qtd_linhas = 1; qtd_linhas <= linhas_legais; qtd_linhas++) {
+        
+        
+        for (int contador_dnmr = 0; contador_dnmr < qtd_linhas; contador_dnmr++) {
+            printf("%d ", numerozinho);  
+            numerozinho++;
+        }
+
+        
+        printf("\n");
+    }
+
+    return 0;
+}
+
 
